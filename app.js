6c91e71286f822bfca1343f96ac8b557f7fc5f48
Edgardo <laraedgardo134@gmail.com>
2024-06-26 22:23:32 -0300

    actualizando mensaje en java ascript

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-let titulo = document.querySelector('h1');ç
+let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Juego del numero secreto';
+
+let parrafo = doucment.querySelector('p');
+parrafo.innerHTML = 'Indica unh numero del 1 al 10';
