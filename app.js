e2f693f454ee6f32ac95e11db4f13e3005fc3e80
Edgardo <laraedgardo134@gmail.com>
2024-06-27 21:32:14 -0300

    optimizando codigo en js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let titulo = document.querySelector('h1');
+/*let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Juego del numero secreto';
 
 let parrafo = document.querySelector('p');
@@ -6,4 +6,16 @@ parrafo.innerHTML = 'Indica un numero del 1 al 10';
 
 function intentoDeUsuario() {
     alert('click desde el boton');
-}
+}*/
+
+    let parrafo = document.querySelector('p');
+    parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+    function asignarTextoElemnto(elemento, texto){
+        let elementoHTML = document.querySelector(elemento);
+        titulo.innerHTML = texto;
+    }
+
+    function intentoDeUsuario() {
+        alert('click desde el boton');
+    }
