ff3c0a94c9a27af6e159d50493a6dde307e79c94
Edgardo <laraedgardo134@gmail.com>
2024-06-27 23:06:30 -0300

    actualizando y optimizando codigo en java script

--- app.js
+++ app.js
@@ -8,14 +8,36 @@ function intentoDeUsuario() {
     alert('click desde el boton');
 }*/
 
-    let parrafo = document.querySelector('p');
-    parrafo.innerHTML = 'Indica un numero del 1 al 10';
+        let numeroSecreto = generarNumeroSecreto();
+        let intentos = 1;
 
-    function asignarTextoElemnto(elemento, texto){
-        let elementoHTML = document.querySelector(elemento);
-        titulo.innerHTML = texto;
-    }
+                function asignarTextoElemento(elemento, texto){
+                    let elementoHTML = document.querySelector(elemento);
+                    elementoHTML.innerHTML = texto;
+                    return;
+                }
 
-    function intentoDeUsuario() {
-        alert('click desde el boton');
-    }
+                    function verificarIntento() {
+                        let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+                        console.log(intentos);
+                    
+                        if (numeroDeUsuario === numeroSecreto) {
+                            asignarTextoElemento('p',`Acertaste el número en ${intentos} 
+                                ${(intentos === 1) ? 'vez' : 'veces'}`);
+                        }else {
+                            if (numeroDeUsuario > numeroSecreto) {
+                                asignarTextoElemento('p','El número secreto es menor');
+                        }else {
+                            asignarTextoElemento('p','El número secreto es mayor');
+                        }
+                        intentos++;
+                        return;
+                        }
+                    }
+
+                function generarNumeroSecreto() {
+                    return  Math.floor(Math.random()*10)+1;
+                }
+
+                asignarTextoElemento('h1','Juego del numero secreto');
+                asignarTextoElemento('p','Indica un numero del 1 al 10');
